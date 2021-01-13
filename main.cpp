42050f6b629326909c5813f4ccd459b7d4d56c15
pereiradaniel <danielpaulpereira@protonmail.com>
2021-01-13 13:27:14 -0500

    Use static_assert.

--- main.cpp
+++ main.cpp
@@ -18,16 +18,21 @@ int main(int argc, char* argv[])
 	// const int size = 9;
 	// short arr[size];
 
-	int val;
-	cin >> val;
-	
-	Foo theFoo{ val };
-	cout << theFoo.m_val;
-	
-	const int constVal = val; // Value cannot be changed
+	//int val;
+	//cin >> val;
+	//
+	//Foo theFoo{ val };
+	//cout << theFoo.m_val;
+	//
+	//const int constVal = val; // Value cannot be changed
 
-	constexpr int size = 9;	// constexpr: the value cannot be changed AND must be known at compile time.
-	short arr[getValue(3)];
+	//constexpr int size = 9;	// constexpr: the value cannot be changed AND must be known at compile time.
+	//short arr[getValue(3)];
+
+	static_assert(getValue(3) == 30, "getValue returns wrong result for param 3.");
+
+	cout << "Done.";
+
 	//for (int i = 0; i < getValue(3); i++)
 	//{
 	//	cout << arr[i];
