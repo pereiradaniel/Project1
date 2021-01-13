4c744c400de551d161dc0c421b19c3b9cfdf3d01
pereiradaniel <danielpaulpereira@protonmail.com>
2021-01-13 12:35:25 -0500

    Updates.

--- utilities.cpp
+++ utilities.cpp
@@ -8,12 +8,14 @@ namespace foo
 	int g_value = 10;
 	void sayHello()
 	{
-		cout << "Hello: [" << g_value << "]\n";
+		static int cnt = 0;	// This variable will exist for the entire running of the program. After the first time this function is running, this declaration is ignored. cnt will be incremented.
+		cnt++;
+		cout << cnt << "Hello: [" << g_value << "]\n";
 	}
 
 	static void sayBye()
 	{
 		cout << "Goodbye\n";
 	}
-	// Static function has external linker. Stays inside this module.
+	// Static function makes this function available only from inside this file.
 }
