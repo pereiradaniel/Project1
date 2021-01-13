2a6191d586a077531af22d4167be23cd3a69b429
pereiradaniel <danielpaulpereira@protonmail.com>
2021-01-13 13:02:20 -0500

    constexpr function

--- main.cpp
+++ main.cpp
@@ -4,20 +4,30 @@
 using namespace std;
 using namespace foo;
 
+constexpr int getValue (int seed) 
+{
+	return seed * 10;
+}
+
 int main(int argc, char* argv[])
 {
+	//sayHello();
+	//g_value = 20;
+	//sayHello();
+
 	// const int size = 9;
 	// short arr[size];
 
 	int val;
 	cin >> val;
+	
 	Foo theFoo{ val };
 	cout << theFoo.m_val;
+	
+	const int constVal = val; // Value cannot be changed
 
-	constexpr int size = 9;
-	short arr[size];
+	constexpr int size = 9;	// constexpr: the value cannot be changed AND must be known at compile time.
+	short arr[getValue(3)];
 
-	//sayHello();
-	//g_value = 20;
-	//sayHello();
+
 }
